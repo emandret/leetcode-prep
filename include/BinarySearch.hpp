49e91a602d3c4fb8d76c51d1aa055f4f26382aee
Ed M <16176054+emandret@users.noreply.github.com>
2026-03-03 14:24:14 +0000

    Create BinarySearch.hpp

---
+++ include/BinarySearch.hpp
@@ -0,0 +1,14 @@
+#ifndef BINARY_SEARCH_HPP_
+#define BINARY_SEARCH_HPP_
+
+#include <vector>
+
+using namespace std;
+
+class BinarySearch
+{
+  public:
+    int search(vector<int>& nums, int target);
+};
+
+#endif // BINARY_SEARCH_HPP_
